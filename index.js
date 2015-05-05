b5885a981a6fbe3bf90318ac41edc48c97eb978a
Andrew Myers (The Masked Coder) <maskedcoder@hush.com>
2015-05-05 18:20:47 -0500

    Implement main function
    - Require gulp-util, little-template,
      and through2

---
+++ index.js
@@ -0,0 +1,42 @@
+'use strict';
+
+var through = require('through2');
+var gutil = require('gulp-util');
+var little = require('little-template');
+var fs = require('fs');
+var path = require('path');
+
+module.exports = function (settings) {
+    settings = (typeof settings == 'function') ? {render: settings} : settings;
+    settings.ext = typeof settings.ext === "undefined" ? ".html" : settings.ext;
+
+    return through.obj(function (file, enc, cb) {
+        if (file.isNull()) {
+            this.push(file);
+            return cb();
+        }
+
+        if (file.isStream()) {
+            this.emit(
+                'error',
+                new gutil.PluginError('gulp-little-template', 'Streaming not supported')
+            );
+        }
+
+        try {
+            file.contents = new Buffer(little(file.contents.toString(), function (templateName, context) {
+              var filePath = (settings.path) ? path.join(settings.path, templateName) : templateName,
+                  templateFile = fs.readFileSync(filePath, 'utf-8');
+
+              return settings.render(templateFile, context, templateName);
+            }));
+            file.path = gutil.replaceExtension(file.path, settings.ext);
+        } catch (err) {
+            this.emit('error', new gutil.PluginError('gulp-little-template', err.toString()));
+          throw err;
+        }
+
+        this.push(file);
+        cb();
+    });
+};
