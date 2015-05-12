c3383f47dca83dc8016642acbc33b295c88851b2
Andrew Myers (The Masked Coder) <maskedcoder@hush.com>
2015-05-12 17:38:52 -0500

    Fix formatting

--- index.js
+++ index.js
@@ -9,49 +9,57 @@ var path = require('path');
 /**
  * Render a string of HTML using Little Template
  *
- * @param {Object|Function}  settings    Plugin settings, or a function to use to render templates. If an object is given, the value for 'render' must be a function to handle rendering templates.
+ * @param {Object|Function}  settings    Plugin settings, or a function to use
+ *                                       to render templates. If an object is
+ *                                       given, the value for 'render' must be
+ *                                       a function to handle rendering
+ *                                       templates.
  * @api public
  */
 module.exports = function (settings) {
-    settings = (typeof settings == 'function') ? {render: settings} : settings;
-    settings.ext = typeof settings.ext === "undefined" ? ".html" : settings.ext;
+  settings = (typeof settings == 'function') ? {render: settings} : settings;
+  settings.ext = typeof settings.ext === 'undefined' ? '.html' : settings.ext;
 
-    // Store the contents of each file, so we don't have to read it more than once
-    var cache = {};
+  // Store the contents of each file, so we don't have to read it more than once
+  var cache = {};
 
-    return through.obj(function (file, enc, cb) {
-        if (file.isNull()) {
-            this.push(file);
-            return cb();
+  return through.obj(function (file, enc, cb) {
+    if (file.isNull()) {
+      this.push(file);
+      return cb();
+    }
+
+    if (file.isStream()) {
+      this.emit(
+        'error',
+        new gutil.PluginError('gulp-little-template', 'Streaming not supported')
+      );
+    }
+
+    try {
+      file.contents = new Buffer(little(file.contents.toString(),
+                                        function (templateName, context) {
+        var filePath = (settings.path) ?
+                      path.join(settings.path, templateName) :
+                      templateName;
+        var templateFile;
+
+        if (cache.hasOwnProperty(filePath)) {
+          templateFile = cache[filePath];
+        } else {
+          templateFile = fs.readFileSync(filePath, 'utf-8');
+          cache[filePath] = templateFile;
         }
 
-        if (file.isStream()) {
-            this.emit(
-                'error',
-                new gutil.PluginError('gulp-little-template', 'Streaming not supported')
-            );
-        }
+        return settings.render(templateFile, context, templateName);
+      }));
+      file.path = gutil.replaceExtension(file.path, settings.ext);
+    } catch (err) {
+      this.emit('error',
+                new gutil.PluginError('gulp-little-template', err.toString()));
+    }
 
-        try {
-            file.contents = new Buffer(little(file.contents.toString(), function (templateName, context) {
-              var filePath = (settings.path) ? path.join(settings.path, templateName) : templateName,
-                  templateFile;
-
-              if (cache.hasOwnProperty(filePath)) {
-                templateFile = cache[filePath];
-              } else {
-                templateFile = fs.readFileSync(filePath, 'utf-8');
-                cache[filePath] = templateFile;
-              }
-
-              return settings.render(templateFile, context, templateName);
-            }));
-            file.path = gutil.replaceExtension(file.path, settings.ext);
-        } catch (err) {
-            this.emit('error', new gutil.PluginError('gulp-little-template', err.toString()));
-        }
-
-        this.push(file);
-        cb();
-    });
+    this.push(file);
+    cb();
+  });
 };
--- test/test.js
+++ test/test.js
@@ -1,16 +1,17 @@
 'use strict';
 
-var fs = require('fs'),
-    should = require('should'),
-    path = require('path');
+var fs = require('fs');
+var should = require('should');
+var path = require('path');
 require('mocha');
 
-var gutil = require('gulp-util'),
-    little = require('../'),
-    mustache = require('./lib/mustache');
+var gutil = require('gulp-util');
+var little = require('../');
+var mustache = require('./lib/mustache');
 
 /**
- * Remove extra whitespace characters from a string, preserving a single space character
+ * Remove extra whitespace characters from a string,
+ * preserving a single space character
  *
  * @param {String}  str    The string to remove whitespace from
  * @return {String}
@@ -21,94 +22,69 @@ var removeExtraWhitespaceChars = function (str) {
 
 describe('gulp-little-template', function () {
 
-    var expectedFile = new gutil.File({
-        path: 'test/expected/output.html',
-        cwd: 'test/',
-        base: 'test/expected',
-        contents: fs.readFileSync('test/expected/output.html')
-    });
+  var expectedFile = new gutil.File({
+    path: 'test/expected/output.html',
+    cwd: 'test/',
+    base: 'test/expected',
+    contents: fs.readFileSync('test/expected/output.html')
+  });
 
-    var expectedFileWithEmbed = new gutil.File({
-        path: 'test/expected/outputWithEmbeddedTemplate.html',
-        cwd: 'test/',
-        base: 'test/expected',
-        contents: fs.readFileSync('test/expected/outputWithEmbeddedTemplate.html')
-    });
+  var expectedFileWithEmbed = new gutil.File({
+    path: 'test/expected/outputWithEmbeddedTemplate.html',
+    cwd: 'test/',
+    base: 'test/expected',
+    contents: fs.readFileSync(
+      'test/expected/outputWithEmbeddedTemplate.html'
+    )
+  });
 
-    it('should produce correct html output when rendering a file', function (done) {
+  it('should produce correct html output when rendering a file',
+     function (done) {
 
-        var srcFile = new gutil.File({
-            path: 'test/fixtures/ok.html',
-            cwd: 'test/',
-            base: 'test/fixtures',
-            contents: fs.readFileSync('test/fixtures/ok.html')
-        });
+       var srcFile = new gutil.File({
+         path: 'test/fixtures/ok.html',
+         cwd: 'test/',
+         base: 'test/fixtures',
+         contents: fs.readFileSync('test/fixtures/ok.html')
+       });
 
-        var stream = little({
-          path: 'test/fixtures/templates',
-          render: function (template, context, templateName) {
-            return mustache.render(template, context);
-          }
-        });
+       var stream = little({
+         path: 'test/fixtures/templates',
+         render: function (template, context, templateName) {
+           return mustache.render(template, context);
+         }
+       });
 
-        stream.on('error', function (err) {
-            should.exist(err);
-            done(err);
-        });
+       stream.on('error', function (err) {
+         should.exist(err);
+         done(err);
+       });
 
-        stream.on('data', function (newFile) {
+       stream.on('data', function (newFile) {
+         should.exist(newFile);
+         should.exist(newFile.contents);
 
-            should.exist(newFile);
-            should.exist(newFile.contents);
+         String(newFile.contents).should.equal(String(expectedFile.contents));
+         done();
+       });
 
-            String(newFile.contents).should.equal(String(expectedFile.contents));
-            done();
-        });
+       stream.write(srcFile);
+       String(path.extname(srcFile.path)).should.equal('.html');
 
-        stream.write(srcFile);
-        String(path.extname(srcFile.path)).should.equal('.html');
+       stream.end();
+     });
 
-      stream.end();
-    });
-
-    it('should throw error when syntax is incorrect', function (done) {
-
-        var srcFile = new gutil.File({
-            path: 'test/fixtures/nok.html',
-            cwd: 'test/',
-            base: 'test/fixtures',
-            contents: fs.readFileSync('test/fixtures/nok.html')
-        });
-
-        var stream = little({
-          path: 'test/fixtures/templates',
-          render: function (template, context, templateName) {
-            return mustache.render(template, context);
-          }
-        });
-
-        stream.on('error', function (err) {
-            should.exist(err);
-            done();
-        });
-
-        stream.write(srcFile);
-        stream.end();
-    });
-
-  it('should produce correct html output with a specific file extension', function (done) {
+  it('should throw error when syntax is incorrect', function (done) {
 
     var srcFile = new gutil.File({
-      path: 'test/fixtures/ok.html',
+      path: 'test/fixtures/nok.html',
       cwd: 'test/',
       base: 'test/fixtures',
-      contents: fs.readFileSync('test/fixtures/ok.html')
+      contents: fs.readFileSync('test/fixtures/nok.html')
     });
-
 
     var stream = little({
       path: 'test/fixtures/templates',
-      ext: '.txt',
       render: function (template, context, templateName) {
         return mustache.render(template, context);
       }
@@ -116,53 +92,84 @@ describe('gulp-little-template', function () {
 
     stream.on('error', function (err) {
       should.exist(err);
-      done(err);
-    });
-
-    stream.on('data', function (newFile) {
-
-      should.exist(newFile);
-      should.exist(newFile.contents);
-
-      String(newFile.contents).should.equal(String(expectedFile.contents));
-      done();
-    });
-
-    stream.write(srcFile);
-    String(path.extname(srcFile.path)).should.equal('.txt');
-
-    stream.end();
-  });
-
-  it('should produce correct html output using embedded templates', function (done) {
-
-    var srcFile = new gutil.File({
-      path: 'test/fixtures/withEmbeddedTemplate.html',
-      cwd: 'test/',
-      base: 'test/fixtures',
-      contents: fs.readFileSync('test/fixtures/withEmbeddedTemplate.html')
-    });
-
-
-    var stream = little({
-      path: 'test/fixtures/templates',
-      render: function (template, context, templateName) {
-        return mustache.render(template, context);
-      }
-    });
-
-    stream.on('data', function (newFile) {
-
-      should.exist(newFile);
-      should.exist(newFile.contents);
-
-      removeExtraWhitespaceChars(String(newFile.contents))
-        .should.equal(removeExtraWhitespaceChars(String(expectedFileWithEmbed.contents)));
       done();
     });
 
     stream.write(srcFile);
     stream.end();
   });
+
+  it('should produce correct html output with a specific file extension',
+     function (done) {
+
+       var srcFile = new gutil.File({
+         path: 'test/fixtures/ok.html',
+         cwd: 'test/',
+         base: 'test/fixtures',
+         contents: fs.readFileSync('test/fixtures/ok.html')
+       });
+
+
+       var stream = little({
+         path: 'test/fixtures/templates',
+         ext: '.txt',
+         render: function (template, context, templateName) {
+           return mustache.render(template, context);
+         }
+       });
+
+       stream.on('error', function (err) {
+         should.exist(err);
+         done(err);
+       });
+
+       stream.on('data', function (newFile) {
+
+         should.exist(newFile);
+         should.exist(newFile.contents);
+
+         String(newFile.contents).should.equal(String(expectedFile.contents));
+         done();
+       });
+
+       stream.write(srcFile);
+       String(path.extname(srcFile.path)).should.equal('.txt');
+
+       stream.end();
+     });
+
+  it('should produce correct html output using embedded templates',
+     function (done) {
+
+       var srcFile = new gutil.File({
+         path: 'test/fixtures/withEmbeddedTemplate.html',
+         cwd: 'test/',
+         base: 'test/fixtures',
+         contents: fs.readFileSync('test/fixtures/withEmbeddedTemplate.html')
+       });
+
+
+       var stream = little({
+         path: 'test/fixtures/templates',
+         render: function (template, context, templateName) {
+           return mustache.render(template, context);
+         }
+       });
+
+       stream.on('data', function (newFile) {
+
+         should.exist(newFile);
+         should.exist(newFile.contents);
+
+         removeExtraWhitespaceChars(String(newFile.contents))
+           .should.equal(removeExtraWhitespaceChars(
+              String(expectedFileWithEmbed.contents)
+         ));
+         done();
+       });
+
+       stream.write(srcFile);
+       stream.end();
+     });
 
 });
